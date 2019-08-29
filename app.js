dd469c9bd7f3c27800e1fe9256e38e90a586353d
Pedro Leda <pedro.henrique.leda@gmail.com>
2019-08-29 16:33:46 -0300

    criação e adicionando pacotes iniciais

---
+++ app.js
@@ -0,0 +1,18 @@
+//carregando modulos
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const app = express();
+//const mongoose = require('mongoose');
+
+//configurações
+
+
+//rotas
+
+
+//outros
+const PORT = 8081;
+app.listen(PORT,() => {
+  console.log("Servidor Rodando! ");
+});

4aa48e10c1c904f757d57ce9f403e6ff3426ddae
Pedro Leda <pedro.henrique.leda@gmail.com>
2019-09-04 15:05:19 -0300

    Adicionado a função de deletar Posts

--- routes/admin.js
+++ routes/admin.js
@@ -227,4 +227,16 @@ router.post("/postagem/edit", (req, res) => {
       res.redirect("/admin/postagens");
     });
 });
+
+router.get("/postagens/deletar/:id", (req, res) => {
+  Postagem.remove({ _id: req.params.id })
+    .then(() => {
+      req.flash("success_msg", "Postagem deletada com sucesso");
+      res.redirect("/admin/postagens");
+    })
+    .catch(err => {
+      req.flash("error_msg", "Houve um erro interno");
+      res.redirect("/admin/postagens");
+    });
+});
 module.exports = router;

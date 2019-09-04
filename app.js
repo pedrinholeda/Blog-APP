cb189225ee1f4e2c84c9a789c509f0c3a92928e4
Pedro Leda <pedro.henrique.leda@gmail.com>
2019-09-04 18:09:29 -0300

    Criado pagina de Postagem e Home Page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
+require("./models/Postagem");
+const Postagem = mongoose.model("postagens");
 
 //configurações
 // Sessão
@@ -46,7 +48,36 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //rotas
 app.get("/", (req, res) => {
-  res.send("Rota principal");
+  Postagem.find()
+    .populate("categoria")
+    .sort({ data: "desc" })
+    .then(postagens => {
+      res.render("index", { postagens: postagens });
+    })
+    .catch(err => {
+      req.flash("error_msg", "Houve um erro interno");
+      res.redirect("/404");
+    });
+});
+
+app.get("/postagem/:slug", (req, res) => {
+  Postagem.findOne({ slug: req.params.slug })
+    .then(postagem => {
+      if (postagem) {
+        res.render("postagem/index", { postagem: postagem });
+      } else {
+        req.flash("error_msg", "Esta postagem não existe");
+        res.redirect("/");
+      }
+    })
+    .catch(err => {
+      req.flash("error_msg", "Houve um erro interno");
+      res.redirect("/");
+    });
+});
+
+app.get("/404", (req, res) => {
+  res.send("Erro 404!");
 });
 
 app.get("/post", (req, res) => {
